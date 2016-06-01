a78ad19549a23e64b6e630aef56f50029a572599
Peter Vingelmann <petya@master.com>
2016-06-01 16:55:20 +0200

    Upgrade ns3 to rev 12138

--- buildbot.py
+++ buildbot.py
@@ -70,7 +70,7 @@ def configure(properties):
     # See revisions here: http://code.nsnam.org/ns-3-dev/log
     os.chdir(ns3_path)
     run_command(['hg', 'pull'])
-    run_command(['hg', 'checkout', '12068'])
+    run_command(['hg', 'checkout', '12138'])
     # Configure ns-3 with the examples enabled
     run_command([sys.executable, 'waf', 'configure', '--enable-examples'])
 
